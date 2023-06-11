7adc204f7e8da77aefe3e2a1c98a5d8f24fd0229
victorhalimm <victorhalim18@gmail.com>
2023-06-11 18:07:21 +0700

    paralax abal"

--- js/about.js
+++ js/about.js
@@ -1,9 +1,13 @@
 var heroText = document.querySelector('.hero-big-text')
 var heroTextHeight = heroText.offsetHeight
 let nav = document.querySelector('.visible-body')
+const parallax = document.querySelector('.parallax')
 
-window.addEventListener("scroll", () => {
-    if (document.documentElement.scrollTop >= heroTextHeight) {
+console.log('fuck')
+
+parallax.addEventListener("scroll", () => {
+    
+    if (parallax.scrollTop >= heroTextHeight) {
         nav.classList.add('nav-below')
     }
     else nav.classList.remove('nav-below')
